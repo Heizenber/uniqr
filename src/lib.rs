6505f316429f377d03e4ace976d192de7e90ce10
Heizenberg <popov.roman91@gmail.com>
2023-10-20 10:58:39 +0300

    added get_args and run functions

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("uniqr")
+        .version("0.1.0")
+        .author("Roman Popov  example@gmail.com")
+        .about("Rust uniqr")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN FILE")
+                .help("Input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .help("Show counts")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let in_file = matches.value_of("in_file").unwrap().to_string();
+    let out_file = matches.value_of("out_file").map(|s| s.to_string());
+    let count = matches.is_present("count");
+
+    Ok(Config {
+        in_file,
+        out_file,
+        count,
+    })
+}
